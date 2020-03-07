8fc7e3bca84585579466eff982eb85aef9b6c421
Josiah Lee <josiahwlee@gmail.com>
2020-03-07 15:03:45 -0800

    finished storing scoring and gameplay

--- source/main.c
+++ source/main.c
@@ -23,10 +23,13 @@
 #include "simAVRHeader.h"
 #endif
 
+const unsigned char FALLING_RATE = 3; // drop every 250ms 5*50ms
+const unsigned char JOYSTICK_SENSITIVITY = 10; // ignore 150ms after reading joystick 15*10ms
+
 // Structs vvv =============================================================================================
 typedef struct _block{
 	int row; // 31 for center
-	int bit; // 2 for topmost bit
+	int bit; // 1 for topmost bit
 } block;
 
 typedef struct _tetrisBlock{
@@ -79,10 +82,30 @@ const unsigned char defaultBoard[64] = {
 	0b11111,	0b11111,	0b11111,	0b11111,	
 };
 
-unsigned char board[64]; // board
+unsigned char board[64] = {
+	0b11111,	0b11111,	0b11111, 	0b11111,		
+	0b10000,	0b00000,	0b00000,	0b00001,
+	0b10000,	0b00000,	0b00000,	0b00001,
+	0b10000,	0b00000,	0b00000,	0b00001,
+	0b10000,	0b00000,	0b00000,	0b00001,
+	0b10000,	0b00000,	0b00000,	0b00001,
+	0b10000,	0b00000,	0b00000,	0b00001,	
+	0b10000,	0b00000,	0b00000,	0b00001,
+
+	0b10000,	0b00000,	0b00000,	0b00001,
+	0b10000,	0b00000,	0b00000,	0b00001,
+	0b10000,	0b00000,	0b00000,	0b00001,
+	0b10000,	0b00000,	0b00000,	0b00001,
+	0b10000,	0b00000,	0b00000,	0b00001,
+	0b10000,	0b00000,	0b00000,	0b00001,
+	0b10000,	0b00000,	0b00000,	0b00001,
+	0b11111,	0b11111,	0b11111,	0b11111,	
+};; // board
 tetrisBlock *tb; // current block
+int score;
 // global vars ^^^ =============================================================================================
 
+// pixel funcs vvv =============================================================================================
 // set a pix to 0
 void clearPix( int row, int bit) {
 	board[row] = board[row] & ~((int)( 1 << bit));
@@ -117,8 +140,15 @@ void movePixRight ( int row, int bit ){
 bool checkBit ( int row, int bit ) {
 	return (board[row] & (1 << bit));
 }
+//
+unsigned char getBit ( int row, int bit ) {
+	if (checkBit (row, bit))
+		return 0b1;
+	return 0b0;
+}
+// pixel funcs ^^^ =============================================================================================
 
-
+// block funcs vvv =============================================================================================
 // sets pix of block to 1
 void setBlock ( block *b ) {
 	setPix( b->row, b->bit );
@@ -231,7 +261,9 @@ void moveBlock ( block *b , int v, int h){
 		}
 	}
 }
+// block funcs ^^^ =============================================================================================
 
+// tetris block funcs vvv =============================================================================================
 // sets pixels of all blocks to 1
 void setTetrisBlock ( tetrisBlock *tb ) {
 	setBlock(tb->b4);
@@ -259,40 +291,43 @@ bool canMoveTetrisBlockRight ( tetrisBlock *tb ){
 	return (canMoveBlockRight(tb->b1) && canMoveBlockRight(tb->b2) && canMoveBlockRight(tb->b3) && canMoveBlockRight(tb->b4));
 }
 // sets pixels below all blocks to 1
-void moveTetrisBlockDown ( tetrisBlock *tb ){
+bool moveTetrisBlockDown ( tetrisBlock *tb ){
 	clearTetrisBlock(tb);
 	if (!canMoveTetrisBlockDown(tb)){
 		setTetrisBlock(tb);
-		return;
+		return false;
 	}
 	moveBlockDown(tb->b4);
 	moveBlockDown(tb->b3);
 	moveBlockDown(tb->b2);
 	moveBlockDown(tb->b1);
+	return true;
 }
 // sets pixels to the left of all blocks to 1
-void moveTetrisBlockLeft ( tetrisBlock *tb ){
+bool moveTetrisBlockLeft ( tetrisBlock *tb ){
 	clearTetrisBlock(tb);
 	if (!canMoveTetrisBlockLeft(tb)){
 		setTetrisBlock(tb);
-		return;
+		return false;
 	}
 	moveBlockLeft(tb->b4);
 	moveBlockLeft(tb->b3);
 	moveBlockLeft(tb->b2);
 	moveBlockLeft(tb->b1);
+	return true;
 }
 // sets pixels to the right of all blocks to 1
-void moveTetrisBlockRight ( tetrisBlock *tb ){
+bool moveTetrisBlockRight ( tetrisBlock *tb ){
 	clearTetrisBlock(tb);
 	if (!canMoveTetrisBlockRight(tb)){
 		setTetrisBlock(tb);
-		return;
+		return false;
 	}
 	moveBlockRight(tb->b4);
 	moveBlockRight(tb->b3);
 	moveBlockRight(tb->b2);
 	moveBlockRight(tb->b1);
+	return true;
 }
 
 // returns pointer to a new tetris block of type b
@@ -348,8 +383,6 @@ tetrisBlock* createTetrisBlock (int b) {
 void rotateTetrisBlock(tetrisBlock *tb) {
 	clearTetrisBlock(tb);
 	tb->rot += 1;
-	LCD_Cursor(25);
-	LCD_WriteData('0' + tb->rot);
 	switch(tb->type) {
 		case 'J':
 			switch ( (tb->rot)%4 ){
@@ -602,18 +635,19 @@ void rotateTetrisBlock(tetrisBlock *tb) {
 	}
 	setTetrisBlock(tb);
 }
+// tetris block funcs ^^^ =============================================================================================
 
+// board funcs vvv =============================================================================================
 // creates custom char at location loc with info p
-void LCDBuildChar(unsigned char loc, unsigned char *p)
-{
- unsigned char i;
- if(loc<8) //If valid address
- {
-  LCD_WriteCommand(0x40+(loc*8)); //Write to CGRAM
-  for(i=0;i<8;i++)
-  LCD_WriteData(p[i]); //Write the character pattern to CGRAM
- }
-  LCD_WriteCommand(0x80); //shift back to DDRAM location 0
+void LCDBuildChar(unsigned char loc, unsigned char *p){
+ 	unsigned char i;
+ 	if(loc<8) //If valid address
+ 	{
+  		LCD_WriteCommand(0x40+(loc*8)); //Write to CGRAM
+		for(i=0;i<8;i++)
+			LCD_WriteData(p[i]); //Write the character pattern to CGRAM
+ 	}
+  	LCD_WriteCommand(0x80); //shift back to DDRAM location 0
 }
 // creates custom char at location loc from board section place
 void buildBoardChar( unsigned char loc , unsigned char place) {
@@ -643,8 +677,157 @@ void displayBoard() {
 		LCD_WriteData(i);
 	}
 }
+// board funcs ^^^ =============================================================================================
 
+// tetris game funcs vvv =============================================================================================
+bool gameOver() {
+	if (checkBit(31,1))
+		return true;
+	return false;
+}
 
+bool canClearLine(int column, int bit) {
+	for ( int i = 0; i < 16; i++){
+		if ( !checkBit(column + i*4, bit) ){
+			return false;
+		}
+	}
+	return true;
+}
+
+bool clearLine (int line) {
+	int column = line/5;
+	int bit = (4 - line%5);
+ 	if (!canClearLine(column, bit)){
+ 		return false;
+ 	}
+ 	for ( int i = 1; i < 15; i++){
+		clearPix(column + i*4, bit);
+	}
+	return true;
+}
+
+void shiftRow(int row, int bit) {
+	switch (bit) {
+		case 0:
+			if (row % 4 ==  0){
+				board[row] = board[row] | getBit(row+1, 4) | 0b10000;
+			}
+			else {
+				board[row] = board[row] | getBit(row+1, 4);
+			}
+			break;
+		case 1:
+			if (row % 4 == 3){
+				board[row] = ( (board[row] & 0b11110) << 1 ) | getBit(row, 4) << 4 | 0b00001 ;
+			}
+			else if (row % 4 ==  0){
+				board[row] = board[row] << 1 | getBit(row+1, 4) | getBit(row, 3) << 3 | getBit(row, 2) << 2 | 0b10000;
+			}
+			else {
+				board[row] = board[row] << 1 | getBit(row+1, 4) | getBit(row, 3) << 3 | getBit(row, 2) << 2 | getBit(row, 4) << 4;
+			}
+			break;
+		case 2:
+			if (row % 4 == 3){
+				board[row] = ( (board[row] & 0b11110) << 1 ) | getBit(row, 4) << 4 | getBit(row, 3) << 3 | 0b00001;
+			}
+			else if (row % 4 ==  0){ 
+				board[row] = board[row] << 1 | getBit(row+1, 4) | getBit(row, 3) << 3 | 0b10000;
+			}
+			else {
+				board[row] = board[row] << 1 | getBit(row+1, 4) | getBit(row, 4) << 4 | getBit(row, 3) << 3;
+			}
+			break;
+		case 3:
+			if (row % 4 == 3){
+				board[row] = ( (board[row] & 0b11110) << 1 ) | getBit(row, 4) << 4 | 0b00001;
+			}
+			else if (row % 4 ==  0){ 
+				board[row] = board[row] << 1 | getBit(row+1, 4) | 0b10000;
+			}
+			else {
+				board[row] = board[row] << 1 | getBit(row+1, 4) | getBit(row, 4) << 4;
+			}
+			break;
+		default: // 4 or -1
+			if (row % 4 == 3){
+				board[row] = ( (board[row] & 0b11110) << 1 ) | 0b00001;
+			}
+			else if (row % 4 ==  0){
+				board[row] = board[row] << 1 | getBit(row+1, 4) | 0b10000;
+			}
+			else {
+				board[row] = board[row] << 1 | getBit(row+1, 4);
+			}	
+			break;
+	}
+}
+
+void shiftLine(int line) {
+	int column = line/5;
+	int bit = (4 - line%5);
+	for (int i = column; i < 4; i++){ // column
+		for ( int j = 1; j < 15; j++){ // row
+			if ( i == column ){
+				shiftRow( (j*4) + i, bit );
+			}
+			else{
+				shiftRow( (j*4) + i, -1 );
+			}
+		}	
+	}
+
+}
+
+// 43210 43210 43210 43210
+// 01234 56789 01234 56789
+void removeLines() {
+	for ( int i = 1; i < 19; i++){
+		if (clearLine(i)){
+			score++;
+			shiftLine(i);
+			i--;
+		}
+	}
+}
+
+void displayScore() {
+	LCD_Cursor(5);
+	LCD_WriteData('S');
+	LCD_WriteData('C');
+	LCD_WriteData('O');
+	LCD_WriteData('R');
+	LCD_WriteData('E');
+	LCD_WriteData(':');
+	LCD_WriteData('0' + score/10);
+	LCD_WriteData('0' + score%10);
+}
+
+int getHighScore() {
+	uint8_t tens = eeprom_read_byte((uint8_t*)46);
+	uint8_t ones = eeprom_read_byte((uint8_t*)56);
+	if (tens > 58 || ones > 58){
+		return 0;
+	}
+	return ((tens-'0')*10 + (ones-'0'));
+}
+
+void setHighScore() {
+	unsigned char t = score/10;
+	unsigned char o = score%10;
+	eeprom_write_byte ((uint8_t*)46 , t + '0');
+	eeprom_write_byte ((uint8_t*)56 , o + '0');
+}
+
+void resetBoard() {
+	for (int i = 0; i < 64; i++){
+		board[i] = defaultBoard[i];
+	}
+}
+// tetris game funcs ^^^ =============================================================================================
+
+// tick funcs vvv =============================================================================================
 signed char x;
 unsigned char key;
 
@@ -668,6 +851,45 @@ int keyPadTick( int state ) {
 				 	x++;
 					tb = createTetrisBlock(x);				
 				}
+				else if (key == '9'){
+				 	for ( int i = 1; i < 15; i++){
+						setPix(i*4, 3);
+					}			
+				}
+				else if (key == '6'){
+				 	for ( int i = 1; i < 15; i++){
+						setPix(i*4, 2);
+					}			
+				}
+				else if (key == '3'){
+				 	for ( int i = 1; i < 15; i++){
+						setPix(i*4, 1);
+					}			
+				}
+				else if (key == 'C'){
+				 	for ( int i = 1; i < 15; i++){
+						setPix(i*4, 0);
+					}			
+				}
+				else if (key == 'B'){
+				 	for ( int i = 1; i < 15; i++){
+						setPix(i*4 + 1, 4);
+					}			
+				}
+				else if (key == '0'){
+				 	eeprom_write_byte ((uint8_t*)46 , '0');	
+				}
+				else if (key == '#'){
+					LCD_Cursor(32);
+				 	LCD_WriteData( eeprom_read_byte((uint8_t*)56));			
+				}
+				else if (key == 'D'){
+					LCD_Cursor(31);
+				 	LCD_WriteData( eeprom_read_byte((uint8_t*)46));			
+				}
+				else if (key == '*'){
+					setHighScore();		
+				}
 				return 2;
 			}
 			return 1;
@@ -686,22 +908,153 @@ int keyPadTick( int state ) {
 }
 
 unsigned char tetrisCnt;
-
 int tetrisTick( int state ) {
 	switch (state) {
 		case 0: 
-			tetrisCnt = 0;
-			return 1; break;
+			LCD_ClearScreen();
+			LCD_Cursor(1);
+			LCD_WriteData('P');
+			LCD_WriteData('R');
+			LCD_WriteData('E');
+			LCD_WriteData('S');
+			LCD_WriteData('S');
+			LCD_Cursor(17);
+			LCD_WriteData('S');
+			LCD_WriteData('T');
+			LCD_WriteData('A');
+			LCD_WriteData('R');
+			LCD_WriteData('T');
+			return 10;
+			break;
+		case 10:
+			if ( !(PINA & 0x08) ){
+				LCD_ClearScreen();
+				tetrisCnt = 0;
+				return 12;
+			}
+			return 10;
+		break;
+		case 12: 
+			LCD_DisplayString(1,(const unsigned char*) "HIGH SCORE:");
+			LCD_WriteData(getHighScore()/10 + '0');
+			LCD_WriteData(getHighScore()%10 + '0');
+			return 11;
+		break;
+		case 11:
+			if ( tetrisCnt > 40){
+				LCD_ClearScreen();
+				resetBoard();
+				score = 0;
+				displayBoard();
+				displayScore();
+				tetrisCnt = 0;
+				tb = createTetrisBlock(rand() % 6);
+				return 1; 
+			}
+			tetrisCnt++;
+			return 11;
+		break;
 		case 1: 	
-			if (tetrisCnt < 20){
+			if ( !(PINA & 0x08) ){
+				LCD_ClearScreen();
+				tetrisCnt = 0;
+				return 0;
+			}
+			if (tetrisCnt < FALLING_RATE){
 				tetrisCnt++;
 			}
 			else {
 				tetrisCnt = 0;
-				moveTetrisBlockDown(tb);
+				if (moveTetrisBlockDown(tb)){
+					tetrisCnt = 0;
+				}
+				else{
+					displayBoard();
+					return 2;
+				}
 			}
 			displayBoard();
 			return 1;
+		break;
+		case 2: 	
+			removeLines();
+			displayBoard();
+			displayScore();
+			return 3;
+		break;
+		case 3: 
+			if (gameOver()){
+				tetrisCnt = 0;
+				return 4;
+			}	
+			tb = createTetrisBlock(rand() % 6);
+			displayBoard();
+			return 1;
+		break;
+		case 4:
+			LCD_ClearScreen();
+			displayBoard();
+			LCD_Cursor(5);
+			LCD_WriteData('G');
+			LCD_WriteData('A');
+			LCD_WriteData('M');
+			LCD_WriteData('E');
+			LCD_Cursor(21);
+			LCD_WriteData('O');
+			LCD_WriteData('V');
+			LCD_WriteData('E');
+			LCD_WriteData('R');
+			tetrisCnt = 0;
+			return 5;
+		break;
+		case 5:
+			if (tetrisCnt > 40){
+				if ( score > getHighScore() ){
+					setHighScore();
+					return 7;
+				}
+				return 6;
+			}
+			tetrisCnt++;
+			return 5;
+		break;
+		case 6:
+			LCD_ClearScreen();
+			LCD_DisplayString(1,  (const unsigned char*) "PRESS START TO");
+			LCD_Cursor(17);
+			LCD_WriteData('R');
+			LCD_WriteData('E');
+			LCD_WriteData('S');
+			LCD_WriteData('T');
+			LCD_WriteData('A');
+			LCD_WriteData('R');
+			LCD_WriteData('T');
+			LCD_WriteData('-');
+			LCD_WriteData('S');
+			LCD_WriteData('C');
+			LCD_WriteData('O');
+			LCD_WriteData('R');
+			LCD_WriteData('E');
+			LCD_WriteData(':');
+			LCD_WriteData('0' + score/10);
+			LCD_WriteData('0' + score%10);
+			resetBoard();
+			return 10;
+		break;
+		case 7:
+			LCD_DisplayString(1, (const unsigned char*) "NEW HIGH SCORE:");
+			LCD_Cursor(17);
+			LCD_WriteData('0' + score/10);
+			LCD_WriteData('0' + score%10);
+			tetrisCnt = 0;
+			return 8;
+		break;
+		case 8:
+			if (tetrisCnt > 40){
+				return 6;
+			}
+			tetrisCnt++;
+			return 8;
 		break;
 		default:
 			return 0;
@@ -713,63 +1066,74 @@ const unsigned short lrMax = 1023;
 const unsigned short udMax = 1023;
 
 unsigned char joystickCnt;
-
+unsigned char temp;
 int joystickTick( int state ) {
+	// read joystick into temp
+	temp = 0;
+	LCD_Cursor(12);
+	if (ADC_read(0) > lrMax*3/4){
+		temp = 1;
+	}
+	else if (ADC_read(0) < lrMax/4){
+		temp = 2;
+	}
+	else if (ADC_read(1) > udMax*3/4){
+		temp = 3;
+	}
+	else if (ADC_read(1) < udMax/4) {
+		temp = 4;
+	}
+
 	switch (state) {
 		case 0: 
 			joystickCnt = 0;
 			return 1; break;
-		case 1: 	
-			LCD_Cursor(12);
-			if (ADC_read(0) > lrMax*3/4){
-				LCD_WriteData('R');
-				moveTetrisBlockRight(tb);
+		case 1: 
+			if ( !(PINA & 0x04) ){
+				while(moveTetrisBlockDown(tb));
+				return 1;
+			}	
+			switch (temp) {
+				case 1: moveTetrisBlockRight(tb); break;
+				case 2: moveTetrisBlockLeft(tb); break;
+				case 3: rotateTetrisBlock(tb); break;
+				case 4: moveTetrisBlockDown(tb); break;
+				default: break;
 			}
-			else if (ADC_read(0) < lrMax/4){
-				LCD_WriteData('L');
-				moveTetrisBlockLeft(tb);
-			}
-			else
-				LCD_WriteData('0');
-			
-			if (ADC_read(1) > udMax*3/4){
-				LCD_WriteData('U');
-				rotateTetrisBlock(tb);
-			}
-			else if (ADC_read(1) < udMax/4) {
-				LCD_WriteData('D');
-				moveTetrisBlockDown(tb);
-			}
-			else
-				LCD_WriteData('0');
-
-			joystickCnt = 0;
-			return 2;
-			break;
-		case 2: // delay 100ms after reading once
-			if (joystickCnt <10){
-				joystickCnt++;
+			if (temp != 0){
+				joystickCnt = 0;
 				return 2;
 			}
-			else{
+			return 1;
+			break;
+		case 2: // delay 10ms * JOYSTICK_SENSITIVITY
+			if (temp == 0 || joystickCnt >= JOYSTICK_SENSITIVITY){
 				joystickCnt = 0;
 				return 1;
 			}
+			joystickCnt++;
+			return 2;
 			break;
 		default:
 			return 0;
 	}
 	return 0;
 }
+// tick funcs ^^^ =============================================================================================
 
 int main(void) {
+	// init ports
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xF0; PORTB = 0x0F; // Keypad Line
 	DDRC = 0xFF; PORTC = 0x00; // LCD data lines
 	DDRD = 0xFF; PORTD = 0x00; // LCD control lines
 
-	board = defaultBoard;
+	// basic variables
+	score = 0;
+	srand(1);
+	// board = defaultBoard;
 
+	// tasks
  	static task task1, task2, task3;
     task *tasks[] = { &task1, &task2, &task3 };
     const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
@@ -780,14 +1144,14 @@ int main(void) {
     task1.TickFct  = &keyPadTick;
 
     task2.state = 0;
-    task2.period = 50;
+    task2.period = 10;
     task2.elapsedTime = task2.period;
-    task2.TickFct  = &tetrisTick;
+    task2.TickFct  = &joystickTick;
 
     task3.state = 0;
-    task3.period = 10;
+    task3.period = 50;
     task3.elapsedTime = task3.period;
-    task3.TickFct  = &joystickTick;
+    task3.TickFct  = &tetrisTick;
 
     unsigned short i;
     unsigned long GCD = tasks[0]->period;
@@ -795,16 +1159,15 @@ int main(void) {
     	GCD = findGCD(GCD, tasks[i]->period);
     }
 
-    x = -1;
-    // uint8_t ByteOfData;
-    // eeprom_write_byte ((uint8_t*)46 , '1');
-	// ByteOfData = eeprom_read_byte((uint8_t*)46);
- 	// LCD_WriteData(ByteOfData);
+	// init LCD and Timer
 	LCD_init();
 	ADC_init();
     TimerSet(GCD);
     TimerOn();
 
+    // counter for tb 
+    x = -1;
+ 	
     while (1) {
     	for ( i = 0 ; i < numTasks ; i++ ) {
     		if ( tasks[i]->elapsedTime == tasks[i]->period ) {
@@ -819,3 +1182,6 @@ int main(void) {
     return 0;
 }
 
+// avrdude -c atmelice_isp -p atmega1284 -B 5  -U flash:w:build/main.hex
+// avrdude -p atmega2560 -P usb -c stk600 -B 5 -U flash:w:main.hex 
+// avrdude -c atmelice_isp -p atmega1284 -c stk600 -B 5  -U flash:w:build/main.hex
